8a43d8dcf8fe6b90b5681f67172b23982ba2ed3a
Jan Kirchner <jan.k.kirchner@gmail.com>
2019-04-02 14:04:57 +0200

    add map component

---
+++ src/components/map.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Map, Marker, Popup, TileLayer } from "react-leaflet";
+
+export default () => {
+  if (typeof window !== "undefined") {
+    const position = [40.73061, -73.935242];
+    return (
+      <Map center={position} zoom={15} style={{ height: "500px" }}>
+        <TileLayer
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+          attribution="..."
+        />
+        <Marker position={position}>
+          <Popup>My Marker</Popup>
+        </Marker>
+      </Map>
+    );
+  }
+  return <p>Map wird geladen</p>;
+};
